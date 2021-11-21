cbe8be8344b267dfc377fd3325b09296f9ac5cd1
Arjun Doel <arjundoel@gmail.com>
2021-11-21 22:02:02 +0000

    firebase setup

---
+++ libs/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCuEKSiHzbT1jPBttfvZcbTROmJnL_xi2A",
+  authDomain: "nextfire-app-2f423.firebaseapp.com",
+  projectId: "nextfire-app-2f423",
+  storageBucket: "nextfire-app-2f423.appspot.com",
+  messagingSenderId: "664494841973",
+  appId: "1:664494841973:web:80c59bcd78da34dc4a983c",
+  measurementId: "G-TLRJV5CWZY"
+};
+
+if (!firebase.getApps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
