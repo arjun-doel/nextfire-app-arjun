76d563b4202d4095d5f27f42ee8ed48b51152fdc
Arjun Doel <arjundoel@gmail.com>
2021-11-22 20:47:32 +0000

    toast added

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import toast from 'react-hot-toast'
 
 export default function Home() {
   return (
     <div >
-      {/* <Loader show /> */}
+      <button onClick={() => toast.success('yo mum')}>Toast me</button>
     </div>
   )
 }
