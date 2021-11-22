160cf40e72aa35afb159cf15d80fd43e799906df
Arjun Doel <arjundoel@gmail.com>
2021-11-22 21:28:01 +0000

    firebase hook fix

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import { Toaster } from 'react-hot-toast'
 import { UserContext } from '../libs/context'
+import { useUserData } from '../libs/hooks'
 
 function MyApp({ Component, pageProps }) {
+
+  const userData = useUserData();
+
   return (
-    <UserContext.Provider value={{ user: {}, username: 'jeff' }}>
+    <UserContext.Provider value={userData}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
